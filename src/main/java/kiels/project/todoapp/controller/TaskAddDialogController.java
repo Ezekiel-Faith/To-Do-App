14a0aaf3b26e1f461d34865e58cf37dd32dfd67a
Ezekie-Faith <ezekielfaith2000@gmail.com>
2025-03-11 19:51:37 +0100

    setting up task dialog UI

---
+++ src/main/java/kiels/project/todoapp/controller/TaskAddDialogController.java
@@ -0,0 +1,16 @@
+package kiels.project.todoapp.controller;
+
+import io.github.palexdev.materialfx.controls.MFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+
+public class TaskAddDialogController {
+	public MFXTextField taskTitleField;
+	public TextArea taskDescription;
+
+	public void handleCancel(ActionEvent actionEvent) {
+	}
+
+	public void handleSubmit(ActionEvent actionEvent) {
+	}
+}
